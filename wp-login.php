0af06642755d4b284d491559f30a9da5be6d5887
Gabriel Sanzone <32177705+gcmms@users.noreply.github.com>
2020-10-29 16:44:29 -0300

    Revert "Criando tela de login (#2)"
    This reverts commit 0219350db9bfd85bb0889463894a3106a344a5f4.

--- wp-login.php
+++ wp-login.php
@@ -21,24 +21,6 @@ if ( force_ssl_admin() && ! is_ssl() ) {
 		exit;
 	}
 }
-if ( force_ssl_admin() && ! is_ssl() ) {
-	if ( 0 === strpos( $_SERVER['REQUEST_URI'], 'http' ) ) {
-		wp_safe_redirect( set_url_scheme( $_SERVER['REQUEST_URI'], 'https' ) );
-		exit;
-	} else {
-		wp_safe_redirect( 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] );
-		exit;
-	}
-}
-if ( force_ssl_admin() && ! is_ssl() ) {
-	if ( 0 === strpos( $_SERVER['REQUEST_URI'], 'http' ) ) {
-		wp_safe_redirect( set_url_scheme( $_SERVER['REQUEST_URI'], 'https' ) );
-		exit;
-	} else {
-		wp_safe_redirect( 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] );
-		exit;
-	}
-}
 
 
 // Redirect to HTTPS login if forced to use SSL.
